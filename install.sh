9be729d6f1bcf7a2e78de73ffd3fc0f3506e0e86
Eimar Fandino <efandino@schubergphilis.com>
2017-03-08 16:42:47 +0100

    adding aliases

--- install.sh
+++ install.sh
@@ -1,133 +1,146 @@
 #!/bin/bash
-
-. /etc/init.d/functions
-
-# Use step(), try(), and next() to perform a series of commands and print
-# [  OK  ] or [FAILED] at the end. The step as a whole fails if any individual
-# command fails.
 #
-# Example:
-#     step "Remounting / and /boot as read-write:"
-#     try mount -o remount,rw /
-#     try mount -o remount,rw /boot
-#     next
-step() {
-    echo -n "$@"
+# . /etc/init.d/functions
+#
+# # Use step(), try(), and next() to perform a series of commands and print
+# # [  OK  ] or [FAILED] at the end. The step as a whole fails if any individual
+# # command fails.
+# #
+# # Example:
+# #     step "Remounting / and /boot as read-write:"
+# #     try mount -o remount,rw /
+# #     try mount -o remount,rw /boot
+# #     next
+# step() {
+#     echo -n "$@"
+#
+#     STEP_OK=0
+#     [[ -w /tmp ]] && echo $STEP_OK > /tmp/step.$$
+# }
+#
+# try() {
+#     # Check for `-b' argument to run command in the background.
+#     local BG=
+#
+#     [[ $1 == -b ]] && { BG=1; shift; }
+#     [[ $1 == -- ]] && {       shift; }
+#
+#     # Run the command.
+#     if [[ -z $BG ]]; then
+#         "$@"
+#     else
+#         "$@" &
+#     fi
+#
+#     # Check if command failed and update $STEP_OK if so.
+#     local EXIT_CODE=$?
+#
+#     if [[ $EXIT_CODE -ne 0 ]]; then
+#         STEP_OK=$EXIT_CODE
+#         [[ -w /tmp ]] && echo $STEP_OK > /tmp/step.$$
+#
+#         if [[ -n $LOG_STEPS ]]; then
+#             local FILE=$(readlink -m "${BASH_SOURCE[1]}")
+#             local LINE=${BASH_LINENO[0]}
+#
+#             echo "$FILE: line $LINE: Command \`$*' failed with exit code $EXIT_CODE." >> "$LOG_STEPS"
+#         fi
+#     fi
+#
+#     return $EXIT_CODE
+# }
+#
+# next() {
+#     [[ -f /tmp/step.$$ ]] && { STEP_OK=$(< /tmp/step.$$); rm -f /tmp/step.$$; }
+#     [[ $STEP_OK -eq 0 ]]  && echo "[  OK  ]" || echo "[ FAILED ]"
+#     echo
+#     return $STEP_OK
+# }
+#
+#
+# # Homebrew Script for OSX
+# echo "Installing brew..."
+# /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+#
+#
+# echo "*********** Install dev stuff ***********"
+#
+# step "install node"
+# try brew install node
+# next
+#
+# step "install git"
+# try brew install git
+# next
+#
+# step "install github-desktop"
+# try brew cask install github-desktop
+# next
+#
+# step "install github-desktop"
+# try brew cask install atom
+# next
+#
+# step "install docker"
+# try brew install docker
+# next
+#
+# step "install docker-compose"
+# try brew install docker-compose
+# next
+#
+# step "install bash-git-prompt"
+# try brew install bash-git-prompt
+# next
+#
+# step "install maven"
+# try brew install maven
+# next
+#
+# step "install maven-completion"
+# try brew install maven-completion
+# next
+#
+#
+# #Communication Apps
+# echo "*********** Communication stuff ***********"
+# step "install slack"
+# try brew cask install slack
+# next
+#
+# step "install skype"
+# try brew cask install skype
+# next
+#
+# step "install lync"
+# try brew cask install microsfot-lync
+# next
+#
+# echo "*********** Web stuff ***********"
+# step "install google-chrome"
+# try brew cask install google-chrome
+# next
+#
+# step "install firefox"
+# try brew cask install firefox
+# next
+#
+# step "install firefox"
+# try brew cask install firefox
+# next
 
-    STEP_OK=0
-    [[ -w /tmp ]] && echo $STEP_OK > /tmp/step.$$
-}
+echo 'if [ -f "$(brew --prefix)/opt/bash-git-prompt/share/gitprompt.sh" ]; then'  >>~/.bash_profile
+echo '  __GIT_PROMPT_DIR=$(brew --prefix)/opt/bash-git-prompt/share'  >>~/.bash_profile
+echo '  source "$(brew --prefix)/opt/bash-git-prompt/share/gitprompt.sh"'  >>~/.bash_profile
+echo 'fi'  >>~/.bash_profile
 
-try() {
-    # Check for `-b' argument to run command in the background.
-    local BG=
+echo '#[Alias]'  >>~/.bash_profile
+echo 'alias ll="ls -al"' >>~/.bash_profile
+echo 'alias gs="git status"'  >>~/.bash_profile
+echo 'alias gclean="git checkout ."'  >>~/.bash_profile
+echo 'alias gwipe="git clean -fdx"'  >>~/.bash_profile
+echo 'alias mica="mvn clean install"'  >>~/.bash_profile
+echo 'alias micant="mvn clean install -DskipTests"'  >>~/.bash_profile
 
-    [[ $1 == -b ]] && { BG=1; shift; }
-    [[ $1 == -- ]] && {       shift; }
-
-    # Run the command.
-    if [[ -z $BG ]]; then
-        "$@"
-    else
-        "$@" &
-    fi
-
-    # Check if command failed and update $STEP_OK if so.
-    local EXIT_CODE=$?
-
-    if [[ $EXIT_CODE -ne 0 ]]; then
-        STEP_OK=$EXIT_CODE
-        [[ -w /tmp ]] && echo $STEP_OK > /tmp/step.$$
-
-        if [[ -n $LOG_STEPS ]]; then
-            local FILE=$(readlink -m "${BASH_SOURCE[1]}")
-            local LINE=${BASH_LINENO[0]}
-
-            echo "$FILE: line $LINE: Command \`$*' failed with exit code $EXIT_CODE." >> "$LOG_STEPS"
-        fi
-    fi
-
-    return $EXIT_CODE
-}
-
-next() {
-    [[ -f /tmp/step.$$ ]] && { STEP_OK=$(< /tmp/step.$$); rm -f /tmp/step.$$; }
-    [[ $STEP_OK -eq 0 ]]  && echo "[  OK  ]" || echo "[ FAILED ]"
-    echo
-    return $STEP_OK
-}
-
-
-# Homebrew Script for OSX
-echo "Installing brew..."
-/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-
-
-echo "*********** Install dev stuff ***********"
-
-step "install node"
-try brew install node
-next
-
-step "install git"
-try brew install git
-next
-
-step "install github-desktop"
-try brew cask install github-desktop
-next
-
-step "install github-desktop"
-try brew cask install atom
-next
-
-step "install docker"
-try brew install docker
-next
-
-step "install docker-compose"
-try brew install docker-compose
-next
-
-step "install bash-git-prompt"
-try brew install bash-git-prompt
-next
-
-step "install maven"
-try brew install maven
-next
-
-step "install maven-completion"
-try brew install maven-completion
-next
-
-
-#Communication Apps
-echo "*********** Communication stuff ***********"
-step "install slack"
-try brew cask install slack
-next
-
-step "install skype"
-try brew cask install skype
-next
-
-step "install lync"
-try brew cask install microsfot-lync
-next
-
-echo "*********** Web stuff ***********"
-step "install google-chrome"
-try brew cask install google-chrome
-next
-
-step "install firefox"
-try brew cask install firefox
-next
-
-step "install firefox"
-try brew cask install firefox
-next
-
-
-echo 'export PATH=/usr/local/bin:$PATH' >>~/.bash_profile
+#shortcuts aliases
+echo 'alias workspace="~/Documents/workspace"'  >>~/.bash_profile
